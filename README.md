876748a08d20be781322f632ba335b5b9b215f89
Neolux Lee <neolux_lee@outlook.com>
2025-06-12 21:52:12 +0800

    update todo doc and strcture

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NLXPy: Library Neoluxi
 
+[中文](README.cn.md)
+
 Toolbox by [Neolux Lee](https://me.neolux.eu.org)
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+deps = []
 cv_deps = ["opencv-python-headless", "numpy", "pillow", "scikit-image"]
-utils_deps = []
-test_deps = ["pytest", "pytest-cov", "pytest-mock"]
-all_deps = cv_deps + utils_deps
+misc_deps = []
+dl_deps = ["torch", "torchvision", "torchaudio", "tqdm", "numpy", "scikit-learn"]
+all_deps = cv_deps + misc_deps + dl_deps
+all_deps = list(set(all_deps))  # 去重
+
+test_deps = ["pytest"]
 
 setup(
     name="nlxpy",
@@ -22,10 +26,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[],
+    install_requires=deps,
     extras_require={
         "cv": cv_deps,
-        "utils": utils_deps,
+        "misc": misc_deps,
+        "dl": dl_deps,
+        "test": test_deps,
         "all": all_deps,
     },
     include_package_data=True,
